57abfc564ee71e473cef139ff865691a0980cb42
natalia <natalia.vergarag@mail.udp.cl>
2020-07-16 11:42:50 -0400

    provider and view composer working for $shopping_cart var

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,17 +3,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\ShoppingCart;
 
 class MainController extends Controller{
   public function home(){
-    $shopping_cart_id= \Session::get('shopping_cart_id');
-
-    // $shopping_cart = ShoppingCart::findOrCreateBySessionID(null);  // usuario not sessioned hardcore
-    $shopping_cart = ShoppingCart::findOrCreateBySessionID($shopping_cart_id);
-
-    \Session::put('shopping_cart_id', $shopping_cart->id);
-    return view('main.home',["shopping_cart" => $shopping_cart]);  
+    return view('main.home');  
   }
 }
   
